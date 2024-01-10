ea76d716b6e0da1e048892693024eea07cefc4e6
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2024-01-10 22:54:55 +0100

    Helped tests.

--- test/service/hook_manager_test.exs
+++ test/service/hook_manager_test.exs
@@ -7,34 +7,30 @@ defmodule Test.Service.HookManagerTest do
     Test.Helper.SetupHelper.setup_base(%{}, :hook_manager_test)
   end
 
-  test "cache key" do
-    assert HookManager.cache_key() === :hooks
-  end
-
   test "register handler", %{hub_id: hub_id} = _contexxt do
     mfa = {:m, :f, :a}
-    HookManager.register_handler(hub_id, :test, mfa)
+    HookManager.register_hook_handlers(hub_id, :test, [mfa])
 
-    assert HookManager.registered_handlers(hub_id) === %{test: [mfa]}
+    assert HookManager.registered_handlers(hub_id, :test) === [mfa]
   end
 
   test "registered handler", %{hub_id: hub_id} = _contexxt do
-    assert HookManager.registered_handlers(hub_id) === %{}
+    assert HookManager.registered_handlers(hub_id, :test) === []
 
     mfa = {:ma, :ff, :aa}
-    HookManager.register_handler(hub_id, :test, mfa)
+    HookManager.register_hook_handlers(hub_id, :test, [mfa])
 
-    assert HookManager.registered_handlers(hub_id) === %{test: [mfa]}
+    assert HookManager.registered_handlers(hub_id, :test) === [mfa]
   end
 
   test "dispatch hooks", %{hub_id: hub_id} = _context do
-    HookManager.register_handler(
+    HookManager.register_hook_handlers(
       hub_id,
       :test_1,
-      {:erlang, :send, [self(), :dispatch_hooks_test_1]}
+      [{:erlang, :send, [self(), :dispatch_hooks_test_1]}]
     )
 
-    HookManager.register_handler(hub_id, :test_2, {:erlang, :send, [self(), :_]})
+    HookManager.register_hook_handlers(hub_id, :test_2, [{:erlang, :send, [self(), :_]}])
 
     HookManager.dispatch_hooks(hub_id, [{:test_1, :hook_data}, {:test_2, :dispatch_hooks_test_2}])
 
@@ -43,15 +39,15 @@ defmodule Test.Service.HookManagerTest do
   end
 
   test "dispatch hook", %{hub_id: hub_id} = _context do
-    HookManager.register_handler(
+    HookManager.register_hook_handlers(
       hub_id,
       :test_1,
-      {:erlang, :send, [self(), :dispatch_hook_test_1]}
+      [{:erlang, :send, [self(), :dispatch_hook_test_1]}]
     )
 
     HookManager.dispatch_hook(hub_id, :test_1, :hook_data)
 
-    HookManager.register_handler(hub_id, :test_2, {:erlang, :send, [self(), :_]})
+    HookManager.register_hook_handlers(hub_id, :test_2, [{:erlang, :send, [self(), :_]}])
     :ok = HookManager.dispatch_hook(hub_id, :test_2, :dispatch_hook_test_2)
 
     assert_receive :dispatch_hook_test_1
