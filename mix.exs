38d9d4d7715e3aecb179bb59fe27375ad054bca5
anuar <anuar.alfetahe@gmail.com>
2025-09-11 23:37:53 +0200

    Disable elector automatic starting.

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule ProcessHub.MixProject do
     [
       {:blockade, "~> 0.2.2"},
       {:hash_ring, "~> 0.4.2"},
-      {:elector, "~> 0.3.0"},
+      # {:elector, "~> 0.3.0", runtime: false}, TODO: revert
+      {:elector, path: "../elector", runtime: false},
       {:ex_doc, "~> 0.34.2", only: :dev, runtime: false},
       {:benchee, "~> 1.2", only: [:dev, :test]}
     ]
