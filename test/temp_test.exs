c47dfee369be5f23f0246bb796b6c54c4be1abb6
anuar <anuar.alfetahe@adm.ee>
2026-02-08 17:30:35 +0200

    Storing state.

--- test/temp_test.exs
+++ test/temp_test.exs
@@ -10,11 +10,6 @@ defmodule Test.TempTest do
   # Total nr of nodes to start (without the main node)
   @nr_of_peers 5
 
-  # Number of new nodes to add during scale-up
-  @peers_to_start 3
-
-  # Number of children to start
-  @child_count 10000
 
   setup_all context do
     context = Map.put(context, :validate_metadata, false)
@@ -26,34 +21,50 @@ defmodule Test.TempTest do
     Bootstrap.bootstrap(context)
   end
 
-  # @tag hub_id: :pubsub_start_rem_test
-  # @tag sync_strategy: :pubsub
-  # @tag validate_metadata: true
-  # @tag listed_hooks: [
-  #        {Hook.post_cluster_join(), :global},
-  #        {Hook.registry_pid_inserted(), :global},
-  #        {Hook.registry_pid_removed(), :global}
-  #      ]
-  # test "pubsub children starting and removing", %{hub_id: hub_id} = context do
-  #   child_count = 30000
-  #   child_specs = Bag.gen_child_specs(child_count, prefix: Atom.to_string(hub_id))
+  @tag hub_id: :static_quroum_test
+  @tag partition_strategy: :static
+  @tag quorum_size: @nr_of_peers + 2
+  @tag quorum_startup_confirm: true
+  @tag listed_hooks: [
+         {Hook.post_cluster_join(), :global},
+         {Hook.post_cluster_leave(), :local},
+         {Hook.post_nodes_redistribution(), :local}
+       ]
+  test "static quorum with min of #{@nr_of_peers + 2} nodes",
+       %{hub_id: hub_id, peer_nodes: peers, listed_hooks: lh} = context do
+    :net_kernel.monitor_nodes(true)
+    # We don't have enough nodes to form the cluster and startup_confirm is set `true`
+    assert ProcessHub.is_partitioned?(hub_id) === true
 
-  #   # Starts children on all nodes.
-  #   Common.sync_base_test(context, child_specs, :add, scope: :global)
+    peers_to_start = @nr_of_peers - 3
+    new_peers = TestNode.start_nodes(peers_to_start, prefix: :static_quorum_test_batch1)
+    peer_names = for {peer, _pid} <- new_peers, do: peer
 
-  #   # Tests if all child_specs are used for starting children.
-  #   Common.validate_registry_length(context, child_specs)
+    Bootstrap.gen_hub(context)
+    |> Bootstrap.start_hubs(peer_names, lh, new_nodes: true, skip_await: true)
 
-  #   # Tests if all child_specs are started on all nodes.
-  #   Common.validate_started_children(context, child_specs)
+    Bag.receive_multiple(peers_to_start, Hook.post_nodes_redistribution())
 
-  #   # Tests children adding and syncing.
-  #   Common.validate_sync(context)
+    # Flush any stale messages from the join phase.
+    Bag.all_messages()
 
-  #   # Stops children on all nodes.
-  #   Common.sync_base_test(context, child_specs, :rem, scope: :global)
+    # We have added `peers_to_start` nodes so our cluster shouldn't be partitioned anymore.
+    assert ProcessHub.is_partitioned?(hub_id) === false
 
-  #   # Tests children removing and syncing.
-  #   Common.validate_sync(context)
-  # end
+    removed_peers = Common.stop_peers(new_peers, 1)
+    new_peers = Enum.filter(new_peers, fn node -> !Enum.member?(removed_peers, node) end)
+    Bag.receive_multiple(1, Hook.post_cluster_leave())
+
+    # We still achive quorum
+    assert ProcessHub.is_partitioned?(hub_id) === false
+
+    removed_peers = Common.stop_peers(new_peers, 1)
+    _new_peers = Enum.filter(peers, fn node -> !Enum.member?(removed_peers, node) end)
+    Bag.receive_multiple(1, Hook.post_cluster_leave())
+
+    # Quorum not achieved
+    assert ProcessHub.is_partitioned?(hub_id) === true
+
+    :net_kernel.monitor_nodes(false)
+  end
 end
